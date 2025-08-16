3b9d867bfa03a08a0756cdc79bfe5dbf272b3f71
Jonathan Hill <jhill@surfmed.com>
2025-08-16 16:07:10 -0400

    vibecoded class boilerplate

---
+++ src/endpoints/mutualFunds.ts
@@ -0,0 +1,10 @@
+import type {AxiosInstance} from "axios";
+
+export default class MutualFunds {
+    apiClient: AxiosInstance;
+    constructor(apiClient: AxiosInstance) {
+        this.apiClient = apiClient;
+    }
+
+    // Endpoint fetching functions starts here
+}
--- src/twelveDataWrapper.ts
+++ src/twelveDataWrapper.ts
@@ -4,13 +4,28 @@ import Advanced from "./endpoints/advanced.ts";
 import {AxiosInstance} from "axios";
 import {buildApiClient} from "./apiClient.ts";
 import Analysis from "./endpoints/analysis.ts";
+import Core from "./endpoints/core.ts";
+import Currencies from "./endpoints/currencies.ts";
+import ETFs from "./endpoints/etfs.ts";
+import Fundamentals from "./endpoints/fundamentals.ts";
+import MutualFunds from "./endpoints/mutualFunds.ts";
+import Reference from "./endpoints/reference.ts";
+import Regulatory from "./endpoints/regulatory.ts";
+import TechnicalIndicators from "./endpoints/technicalIndicators.ts";
 
 export default class TwelveDataWrapper {
     private readonly config: TwelveDataConfig;
     private readonly apiClient: AxiosInstance;
     public readonly advanced: Advanced;
     public readonly analysis: Analysis;
+    public readonly core: Core;
     public readonly currencies: Currencies;
+    public readonly etfs: ETFs;
+    public readonly fundamentals: Fundamentals;
+    public readonly mutualFunds: MutualFunds;
+    public readonly reference: Reference;
+    public readonly regulatory: Regulatory;
+    public readonly technicalIndicators: TechnicalIndicators;
     constructor(config: TwelveDataConfig = configDefaults) {
         if (!config) {
             this.config = configDefaults;
@@ -28,6 +43,14 @@ export default class TwelveDataWrapper {
         // endpoint class binding
         this.advanced = new Advanced(this.apiClient);
         this.analysis = new Analysis(this.apiClient);
+        this.core = new Core(this.apiClient);
+        this.currencies = new Currencies(this.apiClient);
+        this.etfs = new ETFs(this.apiClient);
+        this.fundamentals = new Fundamentals(this.apiClient);
+        this.mutualFunds = new MutualFunds(this.apiClient);
+        this.reference = new Reference(this.apiClient);
+        this.regulatory = new Regulatory(this.apiClient);
+        this.technicalIndicators = new TechnicalIndicators(this.apiClient);
 
     }
 }
